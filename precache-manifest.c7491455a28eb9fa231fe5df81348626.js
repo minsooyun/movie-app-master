837c44ed29f6aa2953a9d6e443aac167f93bc0b9
minsooyun <147911827+minsooyun@users.noreply.github.com>
2024-01-04 16:53:05 +0900

    add build files

---
+++ precache-manifest.c7491455a28eb9fa231fe5df81348626.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e33dde9f4008fcd77f2a5bfbf1f38dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ade7349e3a1bbbe8522",
+    "url": "./static/css/main.3e52b1ba.chunk.css"
+  },
+  {
+    "revision": "8e83cfb4f827c246a364",
+    "url": "./static/js/2.f0a08c44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f0a08c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ade7349e3a1bbbe8522",
+    "url": "./static/js/main.5807c776.chunk.js"
+  },
+  {
+    "revision": "d8814db437b9c92031ee",
+    "url": "./static/js/runtime-main.250135c7.js"
+  }
+]);
